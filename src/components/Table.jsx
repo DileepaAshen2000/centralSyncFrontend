d73949585d263c5934929898a6fdd9e58aef11ab
MaleeshaVidurath <109060887+MaleeshaVidurath@users.noreply.github.com>
2024-02-14 01:18:12 +0530

    create front end of request list

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -2,36 +2,20 @@ import * as React from 'react';
 import { DataGrid } from '@mui/x-data-grid';
 
 const columns = [
-  { field: 'id', headerName: 'ID', width: 70 },
-  { field: 'firstName', headerName: 'First name', width: 130 },
-  { field: 'lastName', headerName: 'Last name', width: 130 },
-  {
-    field: 'age',
-    headerName: 'Age',
-    type: 'number',
-    width: 90,
-  },
-  {
-    field: 'fullName',
-    headerName: 'Full name',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 160,
-    valueGetter: (params) =>
-      `${params.row.firstName || ''} ${params.row.lastName || ''}`,
-  },
+  { field: 'id', headerName: 'RequestID', width: 130 },
+  { field: 'reason', headerName: 'Reason', width: 130 },
+  { field: 'department', headerName: 'Department', width: 200 },
+  { field: 'role', headerName: 'Role', width: 200 },
+  {field: 'status',headerName: 'Status',width: 90,},
+
 ];
 
 const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+  { id: "R001", reason: 'Out of stock', department: 'Technology Department', role: 'Head of Technolgy', status: 'Pending'},
+  { id: "R002", reason: 'Out of stock', department: 'Technology Department', role: 'Head of Technolgy', status: 'Pending'},
+  { id: "R003", reason: 'Out of stock', department: 'Technology Department', role: 'Head of Technolgy', status: 'Accepted'},
+  { id: "R004", reason: 'Out of stock', department: 'Technology Department', role: 'Head of Technolgy', status: 'Rejected'},
+  { id: "R005", reason: 'Out of stock', department: 'Technology Department', role: 'Head of Technolgy', status: 'Rejected'},
 ];
 
 export default function Table() {
--- src/pages/Sample.jsx
+++ src/pages/Sample.jsx
@@ -17,7 +17,8 @@ const Sample = () => {
         <div className='w-screen p-10 ' >
           {/* Enter components here, that you want to insert. */}
           {/* Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi saepe iusto pariatur placeat praesentium sunt quia aut, assumenda minus dignissimos officiis, nostrum optio quasi culpa doloremque hic molestias, ab ullam. Culpa laborum veritatis veniam laboriosam perferendis in a repudiandae nihil, reprehenderit ratione ex itaque maiores suscipit, tempore dolorum ad aperiam nam praesentium magnam iure aut saepe animi. Quidem adipisci, animi, neque dolor sequi eaque, quisquam iste placeat minima quae suscipit mollitia voluptates accusamus excepturi. Totam praesentium sit magni corrupti animi quasi quibusdam exercitationem vel nisi cupiditate tempora, perspiciatis deleniti quas, explicabo non dolorem odio harum soluta unde. Ex, voluptas beatae. */}
-          <h1 className="p-4 text-3xl font-bold">User</h1>
+          <h1 className="p-4 text-3xl font-bold">Request</h1>
+          <h5 className='p-5 text'>Here is a list of all request</h5>
           <Table></Table>
         </div>
       </div>
