5055af33b66673d7068fd63740b705ab9c96a55b
MaleeshaVidurath <109060887+MaleeshaVidurath@users.noreply.github.com>
2024-07-21 22:05:24 +0530

    modifications in inventrory requests

--- src/pages/Request/EmployeeInRequestList.jsx
+++ src/pages/Request/EmployeeInRequestList.jsx
@@ -63,6 +63,12 @@ const EmployeeInRequestList = () => {
   const [loadingRequests, setLoadingRequests] = useState(true);
   const [loadingItems, setLoadingItems] = useState(true);
 
+  //state variables for counts
+  const [pendingCount, setPendingCount] = useState(0);
+  const [acceptedCount, setAcceptedCount] = useState(0);
+  const [rejectedCount, setRejectedCount] = useState(0);
+  const [wantToReturnCount, setWantToReturnCount] = useState(0);
+
   useEffect(() => {
     checkEmployeeStatus();
     fetchRequestsData();
@@ -89,40 +95,38 @@ const EmployeeInRequestList = () => {
       const userId = LoginService.returnUserID();
       const response = await axios.get(`http://localhost:8080/request/user/${userId}`);
       let data = formatRequestsData(response.data);
-  
-      const acceptedItems = data.filter(item => item.status === 'ACCEPTED' || item.status === 'WANT_TO_RETURN_ITEM');
-      const acceptedItemDetailsPromises = acceptedItems.map(item => fetchItemDetails(item.itemId));
-      const acceptedItemNames = await Promise.all(acceptedItemDetailsPromises);
-  
-      const acceptedItemsWithNames = acceptedItems.map((item, index) => ({
-        ...item,
-        itemName: acceptedItemNames[index],
-        no: index + 1, // Separate numbering for accepted items
-      }));
-  
-      setRequestRows(data.map((item, index) => ({ ...item, no: index + 1 }))); // Numbering for requests
-      setLoadingRequests(false);
-  
-      const receivedItems = data.filter(item => item.status === 'RECEIVED');
-      const itemDetailsPromises = receivedItems.map(item => fetchItemDetails(item.itemId));
+
+      const itemDetailsPromises = data.map((item) => fetchItemDetails(item.itemId));
       const itemNames = await Promise.all(itemDetailsPromises);
-  
-      const receivedItemsWithNames = receivedItems.map((item, index) => ({
+
+      const requestDataWithNames = data.map((item, index) => ({
         ...item,
         itemName: itemNames[index],
-        no: index + 1, // Separate numbering for received items
       }));
-  
-      setItemsRows(receivedItemsWithNames.concat(acceptedItemsWithNames));
+
+      setRequestRows(requestDataWithNames.map((item, index) => ({ ...item, no: index + 1 })));
+      setLoadingRequests(false);
+
+      const acceptedItems = requestDataWithNames.filter(item => item.status === 'ACCEPTED' || item.status === 'WANT_TO_RETURN_ITEM');
+      const receivedItems = requestDataWithNames.filter(item => item.status === 'RECEIVED');
+
+      setItemsRows(receivedItems.concat(acceptedItems));
       setLoadingItems(false);
-  
+
+      // Calculate counts
+      setPendingCount(requestDataWithNames.filter(item => item.status === 'PENDING').length);
+      setAcceptedCount(requestDataWithNames.filter(item => item.status === 'ACCEPTED').length);
+      setRejectedCount(requestDataWithNames.filter(item => item.status === 'REJECTED').length);
+      setWantToReturnCount(requestDataWithNames.filter(item => item.status === 'WANT_TO_RETURN_ITEM').length);
+
+
     } catch (error) {
       console.error('Failed to fetch inventory requests:', error);
       setLoadingRequests(false);
       setLoadingItems(false);
     }
   };
-  
+
 
   const formatRequestsData = (data) => {
     return data
@@ -164,13 +168,13 @@ const EmployeeInRequestList = () => {
   };
 
   const requestColumns = [
-    { field: 'no', headerName: 'No:', width: 200 },
-    { field: 'date', headerName: 'Date', width: 200 },
-    { field: 'time', headerName: 'Time', width: 200 },
+    { field: 'reqId', headerName: 'Request Id:', width: 200 },
+    { field: 'date', headerName: 'Created Date', width: 200 },
+    { field: 'itemName', headerName: 'Item Name', width: 200 },
     { field: 'reason', headerName: 'Reason', width: 200 },
-    { 
-      field: 'status', 
-      headerName: 'Status', 
+    {
+      field: 'status',
+      headerName: 'Status',
       width: 200,
       renderCell: (params) => {
         const status = params.value;
@@ -190,13 +194,13 @@ const EmployeeInRequestList = () => {
             break;
         }
         return (
-          <Box 
-            sx={{ 
-              padding: '4px 8px', 
-              borderRadius: '4px', 
-              textAlign: 'center', 
+          <Box
+            sx={{
+              padding: '4px 8px',
+              borderRadius: '4px',
+              textAlign: 'center',
               fontWeight: 'bold',
-              backgroundColor 
+              backgroundColor
             }}
           >
             {status}
@@ -207,13 +211,13 @@ const EmployeeInRequestList = () => {
   ];
 
   const itemsColumns = [
-    { field: 'no', headerName: 'No:', width: 180 },
+    { field: 'reqId', headerName: 'Request Id:', width: 180 },
     { field: 'itemName', headerName: 'Item Name', width: 180 },
-    { field: 'date', headerName: 'Received Date', width: 180},
+    { field: 'date', headerName: 'Created Date', width: 180 },
     { field: 'quantity', headerName: 'Requested Quantity', width: 180 },
-    { 
-      field: 'status', 
-      headerName: 'Status', 
+    {
+      field: 'status',
+      headerName: 'Status',
       width: 210,
       renderCell: (params) => {
         const status = params.value;
@@ -236,13 +240,13 @@ const EmployeeInRequestList = () => {
             break;
         }
         return (
-          <Box 
-            sx={{ 
-              padding: '4px 8px', 
-              borderRadius: '4px', 
-              textAlign: 'center', 
+          <Box
+            sx={{
+              padding: '4px 8px',
+              borderRadius: '4px',
+              textAlign: 'center',
               fontWeight: 'bold',
-              backgroundColor 
+              backgroundColor
             }}
           >
             {status}
@@ -250,13 +254,13 @@ const EmployeeInRequestList = () => {
         );
       }
     },
-    
+
   ];
 
 
   const filteredRequestRows = requestRows
-  .filter(row => row.status !== 'ACCEPTED'&& row.status!=='WANT_TO_RETURN_ITEM')
-  .map((item, index) => ({ ...item, id: index + 1 }));
+    .filter(row => row.status !== 'ACCEPTED' && row.status !== 'WANT_TO_RETURN_ITEM')
+    .map((item, index) => ({ ...item, id: index + 1 }));
 
   const formattedItemsRows = itemsRows.map((item, index) => ({
     ...item,
@@ -273,7 +277,7 @@ const EmployeeInRequestList = () => {
           Create New Inventory Request
         </Button>
       </div>
-      
+
       <TabContext value={value}>
         <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
           <TabList onChange={handleChange} aria-label="lab API tabs example">
@@ -282,23 +286,31 @@ const EmployeeInRequestList = () => {
           </TabList>
         </Box>
         <TabPanel value="1">
-        <SectionHeader title="Inventory Requests" color="#00008B" />
-          <Table 
-            rows={filteredRequestRows} 
-            columns={requestColumns} 
-            loading={loadingRequests} 
-            onRowClick={(params) => navigate(`/employee/in-request-document/${params.row.reqId}`)} 
+          <SectionHeader title="Inventory Requests" color="#00008B" />
+          <Table
+            rows={filteredRequestRows}
+            columns={requestColumns}
+            loading={loadingRequests}
+            onRowClick={(params) => navigate(`/employee/in-request-document/${params.row.reqId}`)}
           />
+          <Box sx={{ display: 'flex', justifyContent: 'space-around', marginTop: '16px' }}>
+            <CountBox title="Pending Requests" count={pendingCount} backgroundColor="#ADD8E6" />
+            <CountBox title="Rejected Requests" count={rejectedCount} backgroundColor="#F08080" />
+          </Box>
         </TabPanel>
 
         <TabPanel value="2">
           <SectionHeader title="Items In My Hand" color="#6a1b9a" />
-          <Table 
+          <Table
             rows={formattedItemsRows}
-            columns={itemsColumns} 
-            loading={loadingItems} 
-            onRowClick={(params) => navigate(`/employee/in-request-document/${params.row.reqId}`)} 
+            columns={itemsColumns}
+            loading={loadingItems}
+            onRowClick={(params) => navigate(`/employee/in-request-document/${params.row.reqId}`)}
           />
+          <Box sx={{ display: 'flex', justifyContent: 'space-around', marginTop: '16px' }}>
+            <CountBox title="Accepted Requests" count={acceptedCount} backgroundColor="#90EE90" />
+            <CountBox title="Want To Return Item Requests" count={wantToReturnCount} backgroundColor="#af5c9b" />
+          </Box>
         </TabPanel>
       </TabContext>
     </Box>
@@ -310,6 +322,26 @@ const SectionHeader = ({ title, color }) => {
   return (
     <Box sx={{ backgroundColor: color, padding: '16px', textAlign: 'center', color: 'white' }}>
       <h2>{title}</h2>
+    </Box>
+  );
+};
+
+// New code for CountBox component
+const CountBox = ({ title, count, backgroundColor }) => {
+  if (count === 0) {
+    return null; // Return null to hide the box if count is 0
+  }
+
+  return (
+    <Box sx={{
+      textAlign: 'center',
+      textStyle: 'bold',
+      padding: '16px',
+      borderRadius: '8px',
+      backgroundColor: backgroundColor
+    }}>
+      <h3>{title}</h3>
+      <p>{count}</p>
     </Box>
   );
 };
