1d305848aaf6138dfcfbd1ea1976d896b05f1b8c
Sachini Sahasra <98446017+Sahasra0108@users.noreply.github.com>
2024-07-21 18:55:21 +0530

    Add modifications

--- src/pages/Ticket/CreateTicket.jsx
+++ src/pages/Ticket/CreateTicket.jsx
@@ -104,7 +104,7 @@ const CreateTicket = () => {
     } else if (name === "description") {
       if (!value) {
         validationErrors.description = "Description is required";
-      } else if (value.length < 30 || value.length > 400) {
+      } else if (value.length < 30 || value.length > 900) {
         validationErrors.description =
           "Description must be between 30 and 400 characters";
       }
@@ -396,6 +396,7 @@ const CreateTicket = () => {
                 size="small"
                 onBlur={handleBlur}
                 error={!!errors.description}
+                maxlength="1000"
                 
               ></textarea>
             <Typography  variant="caption" className="text-gray-500">Please provide clear description about the issue and troubleshooting steps you have already attemped.</Typography>
